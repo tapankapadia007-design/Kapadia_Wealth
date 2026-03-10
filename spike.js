3d3d568fcdde59508b5739c7a106159453671b72
tapankapadia007-design <tapankapadia007@gmail.com>
2026-03-10 09:25:18 +0530

    Create spike.js

---
+++ spike.js
@@ -0,0 +1,31 @@
+async function load(){
+
+let res = await fetch(
+"https://YOUR-BACKEND-URL/spikes"
+)
+
+let data = await res.json()
+
+let html=""
+
+data.forEach(s=>{
+
+let color = s.move>0 ? "lime":"red"
+
+html += `
+<tr>
+<td>${s.symbol}</td>
+<td>${s.price}</td>
+<td style="color:${color}">
+${s.move}%
+</td>
+</tr>
+`
+
+})
+
+document.getElementById("data").innerHTML = html
+
+}
+
+setInterval(load,1000)
